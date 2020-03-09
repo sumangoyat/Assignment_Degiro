df95a7520c488ded4714aa70ff27e7a926e79216
Suman Goyat <sumangoyat@Mandeeps-MacBook-Pro.local>
2020-03-09 19:31:07 +0100

    Test files

---
+++ src/com/solaredge/resources/FindElements.java
@@ -0,0 +1,96 @@
+package com.solaredge.resources;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FindElements {
+	
+	private static WebElement element = null;
+	
+	public static WebElement textbox_name(WebDriver driver) {
+		element = driver.findElement(By.id("edit-name"));
+		return element;
+	}
+
+	public static WebElement textbox_emailaddress(WebDriver driver) {
+		element = driver.findElement(By.id("edit-mail"));
+		return element;
+	}
+	
+	public static WebElement textbox_subj(WebDriver driver) {
+		element = driver.findElement(By.id("edit-subject-0-value"));
+		return element;
+	}
+	
+	public static WebElement textbox_message(WebDriver driver) {
+		element = driver.findElement(By.id("edit-message-0-value"));
+		return element;
+	}
+	
+	public static WebElement textbox_cnaam(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"edit-field-client-address-0-address-given-name\"]"));
+		return element;
+	}
+	
+	public static WebElement textbox_csurnaam(WebDriver driver) {
+		element = driver.findElement(By.id("edit-field-client-address-0-address-family-name"));
+		return element;
+	}
+	
+	public static WebElement textbox_compnaam(WebDriver driver) {
+		element = driver.findElement(By.id("edit-field-client-address-0-address-organization"));
+		return element;
+	}
+	
+	public static WebElement textbox_compadd(WebDriver driver) {
+		element = driver.findElement(By.id("edit-field-client-address-0-address-address-line1"));
+		return element;
+	}
+	
+	public static WebElement textbox_compdropdown(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"edit-field-client-address-0-address-country-code--2\"]"));
+		return element;
+	}
+	
+	public static WebElement textbox_compdropdownSelect(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"edit-field-client-address-0-address-country-code--2\"]"));
+		return element;
+	}
+	
+	public static WebElement textbox_complocal(WebDriver driver) {
+		element = driver.findElement(By.xpath("//input[@data-drupal-selector= 'edit-field-client-address-0-address-locality']"));
+		return element;
+	}
+	
+	public static WebElement textbox_comppost(WebDriver driver) {
+		element = driver.findElement(By.xpath("//input[@data-drupal-selector='edit-field-client-address-0-address-postal-code']"));
+		return element;
+	}
+	
+	public static WebElement textbox_calc(WebDriver driver) {
+		element = driver.findElement(By.id("edit-field-number-0-value"));
+		return element;
+	}
+	
+	public static WebElement textbox_compphone(WebDriver driver) {
+		element = driver.findElement(By.id("edit-field-phone-0-value"));
+		return element;
+	}
+	
+	public static WebElement textbox_stateelement(WebDriver driver) {
+		element = driver.findElement(By.xpath("//select[@data-drupal-selector= 'edit-field-client-address-0-address-administrative-area']"));
+		return element;
+	}
+	
+	public static WebElement textbox_succmessage(WebDriver driver) {
+		element = driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
+		return element;
+	}
+	
+	public static WebElement textbox_submit(WebDriver driver) {
+		element = driver.findElement(By.id("edit-submit"));
+		return element;
+	}
+	
+}
---
+++ src/com/solaredge/test/SolarEdgeQAAutomation.java
@@ -0,0 +1,74 @@
+package com.solaredge.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import com.solaredge.resources.FindElements;
+
+import org.junit.Assert;
+
+public class SolarEdgeQAAutomation {
+	private static WebDriver driver = null;
+	
+	public void invokebrowser(){
+		try {
+			System.setProperty("webdriver.chrome.driver", "./chromedriver");
+			driver=new ChromeDriver();
+			driver.manage().deleteAllCookies();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(400, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(400, TimeUnit.SECONDS);
+			driver.get("https://www.solaredge.com/contact/qa_automation_test");
+			
+			FindElements.textbox_name(driver).sendKeys("Suman Goyat");
+			FindElements.textbox_emailaddress(driver).sendKeys("sumangoyat@gmail.com");
+			FindElements.textbox_subj(driver).sendKeys("Computer Applications");
+			FindElements.textbox_message(driver).sendKeys("Message for today");
+			FindElements.textbox_cnaam(driver).sendKeys("NameClient");
+			FindElements.textbox_csurnaam(driver).sendKeys("SurNameClient");
+			FindElements.textbox_compnaam(driver).sendKeys("Name of the Company");
+			FindElements.textbox_compadd(driver).sendKeys("801 Industrial Building");
+			FindElements.textbox_compdropdown(driver).click();
+			
+			Select dropdown = new Select(FindElements.textbox_compdropdown(driver));
+			dropdown.selectByVisibleText("United States");
+			
+			Thread.sleep(400);
+			
+			FindElements.textbox_complocal(driver).sendKeys("Chelsea");
+			FindElements.textbox_comppost(driver).sendKeys("10001");
+			FindElements.textbox_calc(driver).sendKeys("4");
+			FindElements.textbox_compphone(driver).sendKeys("37387284");
+			FindElements.textbox_stateelement(driver).click();
+			
+			Select statedropdown = new Select(FindElements.textbox_stateelement(driver));
+			statedropdown.selectByVisibleText("New York");
+			
+			FindElements.textbox_submit(driver).click();
+			
+			
+			WebElement msg = FindElements.textbox_succmessage(driver);
+	        String text = msg.getText();
+	        String expectedText = "×\n" +
+	                "Status message\n" +
+	                "Successfully submitted";
+	        Assert.assertEquals(expectedText, text);
+	       
+	        Thread.sleep(800);
+	        driver.close();	
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		SolarEdgeQAAutomation qaAutomation = new SolarEdgeQAAutomation();
+		qaAutomation.invokebrowser();
+		
+	}
+
+}
